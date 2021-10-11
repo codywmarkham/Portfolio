1adf594e951593654d9c03ae70ed377c5134aa8e
High_Lander <codywmarkham@gmail.com>
2021-10-11 00:49:10 -0500

    Projects

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+      title: "Api-Quiz",
+      subtitle: "Javascript,HTML,CSS",
+      description:
+        "A simple WEB API Quiz to test your understanding of WEB API.",
+      image: "./apiquiz.PNG",
+      link: "https://codywmarkham.github.io/Api-Quiz/",
+    },
+    {
+      title: "Budget App",
+      subtitle: "Javascript,HTML,CSS",
+      description:
+        "When you need to budget on the go this app will help you do it. Be it online of offline.",
+      image: "./budget.PNG",
+      link: "https://cody-budget-app.herokuapp.com/",
+    },
+    {
+      title: "Work-Out-Tracker",
+      subtitle: "Javascript,CSS,HTML",
+      description:
+        "When you need an app to track your workouts for the day or week use this easy to use Work Out Tracker!",
+      image: "./workout.PNG",
+      link: "https://cody-work-out-tracker.herokuapp.com/",
+    },
+    {
+      title: "Last Moment Savers",
+      subtitle: "Javascript,Handlebars,CSS,Shell",
+      description:
+        "When you need to plan a party this app will help you plan out all the last second details to help you get through the day. So you get stuck hosting the party for the big event you forgot was happening today, what are you supposed to do? When this predicament occurs you can use the Last Moment Savers app to break down all the details into a manageable agenda to get through the day without a hitch. After using the app you will be able to plan for any event based on food, location, equipment, and attendees or what ever else might come your way.",
+      image: "./lastms.png",
+      link: "https://last-moment-savers.herokuapp.com/",
+    },
+    {
+        title: "1 Music Spot",
+        subtitle: "HTML,Javascript",
+        description:
+          "A site to compile all the best artist from a list of genres.",
+        image: "./brand.png",
+        link: "https://sarah4coulson.github.io/1-music-spot/",
+      },
+      {
+        title: "Weather Board",
+        subtitle: "HTML,Javascript,CSS",
+        description:
+          "A way to check the weather in a city of your choice!",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+      },
+  ];
